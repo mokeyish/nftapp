8f9313d01222df697be0db7e7298e8d909cb79a7
YISH <mokeyish@hotmail.com>
2024-06-19 00:01:47 +0800

    5

--- build.rs
+++ build.rs
@@ -18,26 +18,27 @@ fn build_nftset() -> anyhow::Result<()> {
     if target.ends_with("-musl") {
         let target_dir = env::var_os("OUT_DIR").unwrap();
         let musl_root = Path::new(&target_dir);
-        let include_dir = musl_root.join("x86_64-linux-musl-native").join("include");
+        let target = target.replace("unknown-linux", "linux");
+        let include_dir = musl_root.join(&format!("{target}-native")).join("include");
         if !musl_root.exists() {
             std::fs::create_dir_all(musl_root)?;
         }
         
-        let file = musl_root.join("x86_64-linux-musl-native.tgz");
+        let file = musl_root.join(&format!("{target}-native.tgz"));
         if !file.exists() {
             std::process::Command::new("curl")
-                .args(["-OL", "https://musl.cc/x86_64-linux-musl-native.tgz"])
+                .args(["-OL", &format!("https://musl.cc/{target}-native.tgz")])
                 .current_dir(musl_root)
                 .output()
-                .expect("download https://musl.cc/x86_64-linux-musl-native.tgz failed");
+                .expect(&format!("download https://musl.cc/{target}-native.tgz failed"));
         }
 
         if !include_dir.exists() {
             std::process::Command::new("tar")
-                .args(["-xzf", "x86_64-linux-musl-native.tgz"])
+                .args(["-xzf", &format!("{target}-native.tgz")])
                 .current_dir(musl_root)
                 .output()
-                .expect("untar x86_64-linux-musl-native.tgz failed");
+                .expect(&format!("untar {target}-native.tgz failed"));
         }
 
         build.include(include_dir.as_os_str()); // https://musl.cc/x86_64-linux-musl-native.tgz
